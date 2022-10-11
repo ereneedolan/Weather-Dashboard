d5f55202428c174389bf900ac0698dea084c8218
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-11 18:06:54 -0400

    added API url and started setting up variables

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,9 +2,18 @@
 
 // DATA / STATE
 
+var apiKey = "fe8de900e4d58555c4e94cd5616c4b3d";
+var city = "";
+
 // FUNCTIONS
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
 
 // USER INTERACTIONS
 // A user can click...
+// a user can search for a city to determine a 5 day forecast for that location
+    // once a user searches for a city it is stored in local storage 
 
 // INITIALIZATION
